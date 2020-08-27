a1f05cc70751b0beecaa2091c5cf9c4d00953f02
Antony Simiyu <antonymunyasia993@gmail.com>
2020-08-27 18:44:42 +0300

    It is to style header, about, services and what we do

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,5 @@ $(document).ready(function(){
     $(".services").css('background-image', 'url(assets/backgrounds/s_image.jpg)');
     $(".contact").css('background-image', 'url(assets/backgrounds/c_image.jpg)');
 })
+
+
